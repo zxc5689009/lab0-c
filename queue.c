f8b935ae7c111af942808b50900c5818f89437d6
chovyy <we1lesswang@gmail.com>
2025-03-07 20:40:40 +0800

    Update q_remove_tail function The approach is similar to the one used in the q_remove_head function. The difference is that while q_remove_head uses list_first_entry to get the first node. This function uses list_last_entry to get the last node.
    Change-Id: I4cfd78edc615a0f72e94db0fd10a5d560a5cd3fd

--- queue.c
+++ queue.c
@@ -79,7 +79,16 @@ element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    element_t *node;
+    if (!head)
+        return NULL;
+    node = list_last_entry(head, element_t, list);
+    list_del(&node->list);
+    if (sp) {
+        strncpy(sp, node->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
+    return node;
 }
 
 /* Return number of elements in queue */
