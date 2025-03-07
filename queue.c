0dcf35639c048f7122ff99a38f5bfed2bb75d713
chovyy <we1lesswang@gmail.com>
2025-03-07 20:25:03 +0800

    Update q_remove_head function The function works as follows: First, it checks whether the linked list is empty. If not, it uses the list_first_entry() API to get the first node. Next, it removes the node from linked list. If a buffer is provided, it copies the node's string into this buffer. Finally, the function returns the removed node.
    Change-Id: Ideb612b1d17459007fca7d30cc853c26a85619dd

--- queue.c
+++ queue.c
@@ -64,7 +64,16 @@ bool q_insert_tail(struct list_head *head, char *s)
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    element_t *node;
+    if (!head)
+        return NULL;
+    node = list_first_entry(head, element_t, list);
+    list_del(&node->list);
+    if (sp) {
+        strncpy(sp, node->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
+    return node;
 }
 
 /* Remove an element from tail of queue */
